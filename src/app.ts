0b4671655388c600c0388a0c8cf678fe61d2b1d7
AlvaroNascimento94 <alvarosum@hotmail.com>
2025-10-18 10:05:37 -0300

    feat: Corrige importação do roteador para uso das rotas no aplicativo

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
-import { router } from './routes'
+import { routes } from './routes/index'
 import { errorHandler } from './middlewares/errorHandler'
 
 const app = express()
@@ -9,7 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.use(router)
+app.use(routes)
 app.use(
     '/files',express.static(path.resolve(__dirname,"..",'tmp'))
 )
