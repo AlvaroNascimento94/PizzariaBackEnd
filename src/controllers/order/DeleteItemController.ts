e10099653d59a38fec8c7fb7ab4052270bd7803a
AlvaroNascimento94 <alvarosum@hotmail.com>
2025-10-18 10:02:14 -0300

    refactor: Melhora a formatação e a estrutura do controlador e serviço de exclusão de item

--- src/controllers/order/DeleteItemController.ts
+++ src/controllers/order/DeleteItemController.ts
@@ -1,15 +1,16 @@
 import { Response, Request } from "express";
 import { DeleteItemService } from "../../services/order/DeleteItemService";
 
-class DeleteItemController{
-    async handle(req:Request, res :Response){
-        const itemId = req.query.itemId as string
+class DeleteItemController {
+  async handle(req: Request, res: Response) {
+    const itemId = req.query.itemId as string;
 
-        const deleteItem = new DeleteItemService()
+    const deleteItem = new DeleteItemService();
 
-        const order = await deleteItem.execute({itemId})
-        return res.json(order)
-    }
+    const result = await deleteItem.execute({ itemId });
+    
+    return res.json(result);
+  }
 }
 
-export {DeleteItemController}
+export { DeleteItemController };
--- src/services/order/DeleteItemService.ts
+++ src/services/order/DeleteItemService.ts
@@ -1,17 +1,37 @@
 import prismaClient from "../../prisma";
 
-interface IDeleteItem{
-    itemId: string
+interface IDeleteItem {
+  itemId: string;
 }
 
-class DeleteItemService{
-    async execute({itemId}: IDeleteItem){
-        const order = await prismaClient.item.delete({
-            where: {
-                id: itemId
-            }
-        })
-        return order
+class DeleteItemService {
+  async execute({ itemId }: IDeleteItem) {
+
+    const item = await prismaClient.orderProduct.findUnique({
+      where: { id: itemId },
+      include: {
+        order: true,
+      },
+    });
+
+    if (!item) {
+      throw new Error("Item não encontrado");
     }
+
+    if (!item.order.draft) {
+      throw new Error(
+        "Não é possível remover itens de um pedido já enviado para a cozinha"
+      );
+    }
+
+    const deletedItem = await prismaClient.orderProduct.delete({
+      where: {
+        id: itemId,
+      },
+    });
+
+    return deletedItem;
+  }
 }
-export{DeleteItemService}
+
+export { DeleteItemService };
